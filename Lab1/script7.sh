d30c7b77cefcbd00d91fcaf5e0789b9fd3e16032
Evgeny Borisyuk <125742175+Doney888@users.noreply.github.com>
2025-05-07 17:11:10 +0300

    Update script7.sh

--- Lab1/script7.sh
+++ Lab1/script7.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-
+ 
 for i in $(cat wget-pdfs)
 do
     wget $i
 done
-
-ans = 0
-
+ 
+let ans=0
+ 
 for i in $(ls)
 do
-    if [file -N $i | grep "PDF document" != ""];
+    if [[ "$(file -N $i)" == *PDF* ]];
     then
-        echo &(wc -c $i | awc '{print 1}')
+
+        prom=$(wc -c $i | awk '{print $1}')
+
+        let ans=$ans+$prom
     fi
 done
+ 
+echo $ans
